7d599497d23c823103a7bd8d0deea9e5971d048f
Mateus de Souza <mateusdesouzapaula97@gmail.com>
2021-03-03 16:11:53 -0300

    add: WalletBox component

---
+++ src/components/WalletBox/index.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+import arrowDown from "../../assets/arrow-down.svg";
+import arrowUp from "../../assets/arrow-up.svg";
+import dollar from "../../assets/dollar.svg";
+import CountUp from "react-countup";
+
+interface IWalletBoxProps {
+  title: string;
+  amount: number;
+  footerLabel: string;
+  icon: "arrowDown" | "arrowUp" | "dollar";
+  color: string;
+}
+
+interface IContainerProps {
+  color: string;
+}
+
+const WalletBox: React.FC<IWalletBoxProps> = ({
+  title,
+  amount,
+  footerLabel,
+  icon,
+  color,
+}) => {
+  const iconSelected = () => {
+    switch (icon) {
+      case "dollar":
+        return dollar;
+      case "arrowUp":
+        return arrowUp;
+      case "arrowDown":
+        return arrowDown;
+      default:
+        return undefined;
+    }
+  };
+
+  return (
+    <Container color={color}>
+      <span>{title}</span>
+      <h1>
+        <CountUp
+          end={amount}
+          prefix={"R$ "}
+          separator="."
+          decimal=","
+          decimals={2}
+          preserveValue={true}
+        />
+      </h1>
+      <small>{footerLabel}</small>
+      <img src={iconSelected()} alt={title} />
+    </Container>
+  );
+};
+
+const Container = styled.div<IContainerProps>`
+  width: 32%;
+  height: 176px;
+
+  background: ${(props) => props.color};
+  color: ${(props) => props.theme.colors.white};
+  border-radius: 0.25rem;
+  padding: 0.5rem 1rem;
+  overflow: hidden;
+
+  position: relative;
+
+  > img {
+    position: absolute;
+    bottom: -1.5rem;
+    right: -1.5rem;
+    opacity: 0.3;
+    height: 110%;
+  }
+
+  > span {
+    font-size: 1.125rem;
+    font-weight: 500;
+  }
+
+  > small {
+    font-size: 0.675rem;
+    position: absolute;
+    bottom: 1rem;
+  }
+`;
+
+export default WalletBox;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,25 +1,121 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import styled from "styled-components";
+//? Importando o Aray de objetos que serão listados
+import gains from "../../repositories/gains";
+import expenses from "../../repositories/expenses";
 import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
+import listOfMonths from "../../utils/months";
+import WalletBox from "../../components/WalletBox";
 
-const Dashboard: React.FC = () => {
-  const options = [
-    {
-      value: "Mateus",
-      label: "Mateus",
-    },
-  ];
+interface IRoutParams {
+  match: {
+    params: {
+      type: string;
+    };
+  };
+}
+
+const Dashboard: React.FC<IRoutParams> = ({ match }) => {
+  const [monthSelected, setMonthSelected] = useState<String>(
+    String(new Date().getMonth() + 1)
+  );
+  const [yearSelected, setYearSelected] = useState<String>(
+    String(new Date().getFullYear())
+  );
+
+  //! Desestruturação
+  const { type } = match.params;
+
+  //! Mudança do 'title' de acordo com o path
+  const params = useMemo(() => {
+    return type === "entry-balance"
+      ? { title: "Entradas", lineColor: "#f7931b" }
+      : { title: "Saídas", lineColor: "#e44c4e" };
+  }, [type]);
+
+  const listData = useMemo(() => {
+    return type === "entry-balance" ? gains : expenses;
+  }, [type]);
+
+  const years = useMemo(() => {
+    let uniqueYears: number[] = [];
+
+    listData.forEach((item) => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+
+      if (!uniqueYears.includes(year)) {
+        uniqueYears.push(year);
+      }
+    });
+
+    return uniqueYears.map((year) => {
+      return {
+        value: year,
+        label: year,
+      };
+    });
+  }, [listData]);
+
+  const months = useMemo(() => {
+    return listOfMonths.map((month, index) => {
+      return {
+        value: index + 1,
+        label: month,
+      };
+    });
+  }, []);
 
   return (
     <Container>
       <ContentHeader title="Dashboard" lineColor="#f7931b">
-        <SelectInput options={options} onChange={() => {}} />
+        <SelectInput
+          options={months}
+          onChange={(e) => setMonthSelected(e.target.value)}
+          defaultValue={monthSelected.toString()}
+        />
+        <SelectInput
+          options={years}
+          onChange={(e) => setYearSelected(e.target.value)}
+          defaultValue={yearSelected.toString()}
+        />
       </ContentHeader>
+      <Content>
+        <WalletBox
+          title="Saldo"
+          amount={150.0}
+          footerLabel="Atualizado com base na entradas e saídas"
+          icon="dollar"
+          color="#4e41f0"
+        />
+
+        <WalletBox
+          title="Entradas"
+          amount={5000.0}
+          footerLabel="Atualizado com base na entradas e saídas"
+          icon="arrowUp"
+          color="#f7931b"
+        />
+
+        <WalletBox
+          title="Saídas"
+          amount={485.0}
+          footerLabel="Atualizado com base na entradas e saídas"
+          icon="arrowDown"
+          color="#e44c4e"
+        />
+      </Content>
     </Container>
   );
 };
 
 const Container = styled.div``;
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 export default Dashboard;
