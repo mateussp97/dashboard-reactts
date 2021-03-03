3019052f714b708cfffd7c0bc5ef579953fe7b12
Mateus de Souza <mateusdesouzapaula97@gmail.com>
2021-03-03 20:32:40 -0300

    upgrade: refactoring code

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import ImageLogo from "../../assets/logo.svg";
 import {
   MdDashboard,
@@ -6,7 +7,6 @@ import {
   MdArrowUpward,
   MdExitToApp,
 } from "react-icons/md";
-import { Container, Header, Logo, Title, Menu, Link } from "./styles";
 
 const Aside: React.FC = () => {
   return (
@@ -36,5 +36,67 @@ const Aside: React.FC = () => {
     </Container>
   );
 };
+
+const Container = styled.div`
+  grid-area: AS;
+
+  background: ${(props) => props.theme.colors.secondary};
+
+  padding-top: 1rem;
+
+  border-right: 1px solid ${(props) => props.theme.colors.gray};
+`;
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Logo = styled.img`
+  margin-right: 1rem;
+`;
+
+const Title = styled.h3`
+  color: ${(props) => props.theme.colors.white};
+`;
+
+const Menu = styled.nav`
+  margin-top: 5rem;
+  margin-left: 2.5rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+const Link = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: all 0.3s;
+
+  font-size: 1.25rem;
+  font-weight: 300;
+
+  > svg {
+    margin-right: 0.5rem;
+    font-size: 1rem;
+  }
+
+  color: ${(props) => props.theme.colors.info};
+
+  margin-bottom: 2rem;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
 
 export default Aside;
