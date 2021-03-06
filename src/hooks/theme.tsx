fbdb6b2bd56e12d750f14c3130aaa5659fd800e3
Mateus de Souza <mateusdesouzapaula97@gmail.com>
2021-03-06 17:26:13 -0300

    upgrade: storing toggle theme value on localStorage

--- src/hooks/theme.tsx
+++ src/hooks/theme.tsx
@@ -31,15 +31,31 @@ const ThemeContext = createContext<IThemeContext>({} as IThemeContext);
 
 //? Criação do provider
 const ThemeProvider: React.FC = ({ children }) => {
-  //! O estado 'theme' que inicia com o tema dark
-  const [theme, setTheme] = useState<ITheme>(dark);
+  //! O estado 'theme' que inicia com o valor salvo anteriormente no localStorage, caso não encontre o 'theme', será utilizado como padrão o theme dark
+  const [theme, setTheme] = useState<ITheme>(() => {
+    //! É criado uma const que recebe o valor do item pego no localStorage de acordo com a 'key' que foi atribuída anteriormente
+    const themeSaved = localStorage.getItem("@dashboard-reactts:theme");
+    //! Se esse 'themeSaved' for verdadeiro, transforma o 'themeSaved' de texto para objeto JSON e o retorna
+    if (themeSaved) {
+      //! Transforma de texto para objeto JSON
+      return JSON.parse(themeSaved);
+    } else {
+      //! Caso 'themeSaved' for falso, retorna o 'theme' dark como padrão
+      return dark;
+    }
+  });
 
   //! A função que fará a troca do tema
   const toggleTheme = () => {
     if (theme.title === "dark") {
       setTheme(light);
+      //! Salvando o theme selecionado no localStorage, o setItem serve para inserir os dados e o que você quer salvar
+      //! Ele recebe como primeiro parâmetro uma 'key' que você escolhe o nome
+      //! E de segundo parâmetro, o que vai ser salvo. Nesse caso estamos convertendo para texto, para que possa ser salvo
+      localStorage.setItem("@dashboard-reactts:theme", JSON.stringify(light));
     } else {
       setTheme(dark);
+      localStorage.setItem("@dashboard-reactts:theme", JSON.stringify(dark));
     }
   };
 
